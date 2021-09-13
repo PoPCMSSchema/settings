0bd703acd54b66831b620bf8840bc0ace0fda51a
leoloso <leo@getpop.org>
2021-09-13 08:00:19 +0000

    Merge pull request #1033 from leoloso/Move-FieldResolvers-under-ObjectType-and-InterfaceType
    Move FieldResolvers under folders `ObjectType` and `InterfaceType`

--- src/FieldResolvers/ObjectType/RootFieldResolver.php
+++ src/FieldResolvers/ObjectType/RootFieldResolver.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace PoPSchema\Settings\FieldResolvers;
+namespace PoPSchema\Settings\FieldResolvers\ObjectType;
 
-use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
+use PoP\ComponentModel\FieldResolvers\ObjectType\AbstractDBDataFieldResolver;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
 use PoP\Engine\TypeResolvers\ObjectType\RootTypeResolver;
